9b301ddcddaeb3b49ed9aff451bc6a4950338590
Romain <rmtestard@gmail.com>
2019-02-07 08:48:42 +0100

    starter

---
+++ pc.sh
@@ -0,0 +1,8 @@
+lvl 0 --> lvl 1
+// change the url
+http://www.pythonchallenge.com/pc/def/1.html
+// answer 2**38
+// get the result 274877906944, then set the url to 
+http://www.pythonchallenge.com/pc/def/274877906944.html
+
+lvl 1 --> lvl 2
